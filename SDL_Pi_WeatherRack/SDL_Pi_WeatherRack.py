f4def342d04c73de8a3aee91748f654169db148d
henryse <henryse@hotwire.com>
2016-11-06 13:53:34 -0800

    Better stack formating

--- SDL_Pi_WeatherRack/SDL_Pi_WeatherRack.py
+++ SDL_Pi_WeatherRack/SDL_Pi_WeatherRack.py
@@ -203,13 +203,7 @@ class SDL_Pi_WeatherRack:
                     self.ads1015 = ADS1x15(ic=ADS1115, address=0x48)
 
         except TypeError as e:
-            # in Python 2, stderr is also unbuffered
-            print >> sys.stderr, traceback.format_exc()
-            # in Python 2, you can also from __future__ import print_function
-            # print(traceback.format_exc(), file = sys.stderr)
-            # or as the top answer here demonstrates, use:
             traceback.print_exc()
-            # which also uses stderr.
             print e.message
             config.ADS1015_Present = False
             config.ADS1115_Present = False
