2000baf5f65e94d563cb0e4e934defa48b738b08
henryse <henryse@hotwire.com>
2016-11-06 18:12:54 -0800

    Try again to get wether service.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sudo pip install -r requirements.txt
 
-sudo python WeatherServer.py --host=0.0.0.0 --port=80
+export FLASK_APP=/home/pi/SDL_Pi_WeatherBoard/WeatherServer.py
+flash run --host=0.0.0.0 --port=80
